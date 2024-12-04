20a7852c02658fdada7d2256a4309833a4bd7d00
omar-rayyan <omarrayyan999@gmail.com>
2024-12-05 00:14:06 +0200

    Map of the Hasmatique assignment done.

---
+++ _java_fundamentals/MapOfTheHashmatique/TrackList.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrackList {
+    public static void main(String[] args) {
+        HashMap<String, String> trackList = new HashMap<>();
+
+        trackList.put("Uprising", "They will not force us, they will stop degrading us...");
+        trackList.put("Supermassive Black Hole", "You caught me under false pretenses...");
+        trackList.put("Starlight", "Far away, this ship is taking me far away...");
+        trackList.put("Plug In Baby", "My plug in baby, crucifies my enemies...");
+
+        String trackTitle = "Starlight";
+        String lyrics = trackList.get(trackTitle);
+        System.out.println("Lyrics for '" + trackTitle + "': " + lyrics);
+        System.out.println();
+
+        System.out.println("Track List:");
+        for (Map.Entry<String, String> track : trackList.entrySet()) {
+            System.out.println("Track: " + track.getKey() + " | Lyrics: " + track.getValue());
+        }
+    }
+}
