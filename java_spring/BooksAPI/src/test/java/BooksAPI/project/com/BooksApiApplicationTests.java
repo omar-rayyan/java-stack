bc142df037f71fe69b43564e8da088bbe079ff6c
omar-rayyan <omarrayyan999@gmail.com>
2024-12-16 15:16:55 +0200

    BooksAPI assignment finished.

---
+++ java_spring/BooksAPI/src/test/java/BooksAPI/project/com/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package BooksAPI.project.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
